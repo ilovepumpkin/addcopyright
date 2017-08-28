0eb310384b8503c18563bbe14743dcf23a9b5122
shenrui <ilovepumpkin@126.com>
2017-08-28 13:16:47 +0800

    fix the loop file missing issue

--- index.js
+++ index.js
@@ -21,45 +21,62 @@ var argv = require('yargs')
     .help('help')
     .argv;
 
-    
+
 var glob = require("glob");
 var fs = require("fs");
 var loop = require("serial-loop");
 
-var errorHandler = function (error) {
-    if(error) {
+var errorHandler = function(error) {
+    if (error) {
         console.log(chalk.red(error));
         process.exit()
     }
 }
 
-if(!Array.isArray(argv.f)) {
-    errorHandler('Files should be an Array'); 
+if (!Array.isArray(argv.f)) {
+    errorHandler('Files should be an Array');
 }
 
-fs.readFile(argv.c, function (error, copyright) {
+fs.readFile(argv.c, function(error, copyright) {
     if (error) {
         return errorHandler(error);
     }
 
-    argv.f.forEach(function (input) {
-        glob(input, function (er, files) {
-            loop(files.length, each, errorHandler);
-            function each (done, i) {
-                if (fs.lstatSync(files[i]).isFile()) {
-                    fs.readFile(files[i], function (error, buffer) {
+    argv.f.forEach(function(input) {
+        glob(input, function(er, files) {
+            files.forEach(targetFile => {
+                if (fs.lstatSync(targetFile).isFile()) {
+                    fs.readFile(targetFile, function(error, buffer) {
                         if (error) {
-                            return done(error);
+                            return errorHandler(error);
                         }
-                        if (buffer.indexOf(copyright) === -1) {                        
-                            console.log(chalk.green(files[i]));
-                            fs.writeFile(files[i], copyright + buffer,done)
+                        if (buffer.indexOf(copyright) === -1) {
+                            console.log(chalk.green(targetFile));
+                            fs.writeFile(targetFile, copyright + buffer, errorHandler)
                         } else {
-                             console.log(chalk.yellow(files[i]));
+                            console.log(chalk.yellow(targetFile));
                         }
                     });
                 }
-            }
+            })
+
+            // loop(files.length, each, errorHandler);
+
+            // function each(done, i) {
+            //     if (fs.lstatSync(files[i]).isFile()) {
+            //         fs.readFile(files[i], function(error, buffer) {
+            //             if (error) {
+            //                 return done(error);
+            //             }
+            //             if (buffer.indexOf(copyright) === -1) {
+            //                 console.log(chalk.green(files[i]));
+            //                 fs.writeFile(files[i], copyright + buffer, done)
+            //             } else {
+            //                 console.log(chalk.yellow(files[i]));
+            //             }
+            //         });
+            //     }
+            // }
         })
     });
 });
